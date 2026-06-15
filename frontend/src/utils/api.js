4204e01f8384e3380e2dd5e262ff3fc651819589
Kartik96643 <kartikscorner@gmail.com>
2026-06-15 11:07:26 +0530

    Add render url as baseURL

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://hrms-p8zi.onrender.com/api',
 });
 
 // Request interceptor to attach JWT token if available
